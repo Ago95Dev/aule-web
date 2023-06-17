83553ee88d9939e0a9f8a4e28e27fe6f739b7176
Agostino <65028567+Ago95Dev@users.noreply.github.com>
2023-06-17 13:39:41 +0200

    Frontend Informazioni Evento

--- src/main/webapp/res/event.js
+++ src/main/webapp/res/event.js
@@ -43,7 +43,11 @@ $(document).ready(function () {
                 '<td>' + event["description"] + '</td>' +
                 '<td>' + event["type"] + '</td>' +
                 //'<td>' + event["email"] + '</td>' +
-                '</tr>';
+                // Da fare getInformazioniEvento 
+                // Da fare setUpdateEventoForm
+                '<td class="text-center">' +'<a style="text-decoration: none;" href="#" onClick="#getInformazioniEvento(' + response[key]["id"] + ')" data-bs-toggle="modal" data-bs-target="#infoEventoModal" class="text-reset" tabindex="-1"> <button class="btn btn-secondary"><i class="fa-solid fa-circle-info fa-lg"></i></button>' + ' '
+                +'<a style="text-decoration: none;" href="#" onClick="#updateEventForm(' + response[key]["id"] + ')" data-bs-toggle="modal" data-bs-target="#updateEventModal" class="text-reset" tabindex="-1"><button class="btn btn-warning" data-bs-toggle="modal" id="updateButtonShow" data-bs-target="#updateAulaModal"><i class="fa-solid fa-pen-to-square"></i></button>'+
+                '</tr>' + '<td class="text-end"></td>';
               $('#e-table-body').append(row);
             });
           },
@@ -173,6 +177,8 @@ $('#eventType').change(function (event) {
         $('#courseIdEvent').prop("disabled", true);
 
     }
+
+   
 });
 
 $('#recurrentCheckbox').change(function (event) {
@@ -187,3 +193,44 @@ $('#recurrentCheckbox').change(function (event) {
     }
     console.log(value);
 });
+
+function getInformazioniEvento(event_id) {
+    console.log(event_id);
+
+$.ajax({
+url: 'rest/event/' + event_id,
+type: 'GET',
+success: function (response) {
+//rimozione righe statiche
+$('#e-table-body-info').empty();
+/*
+var equipment = "";
+var name = "Non assegnato";
+response["Equipments"].forEach(element => equipment = equipment + "" + element + " - ");
+equipment = equipment.slice(0, -2);
+if (typeof response["Group"][0] !== "undefined") {
+    name = response["Group"][0]["name"];
+
+     id 	name 	date 	
+     start_time 	end_time 	description 	
+     type 	email 	course_id 	classroom_id 	
+} */
+
+var view =
+        '<tr>' + '<td>' + 'Nome ' + '</td>' + '<td>' + response["name"] + '</td>' + '</tr>' +
+        '<tr>' + '<td>' + 'Data ' + '</td>' + '<td>' + response["date"] + '</td>' + '</tr>' +
+        '<tr>' + '<td>' + 'Data Inizio ' + '</td>' + '<td>' + response["start_time"] + '</td>' + '</tr>' +
+        '<tr>' + '<td>' + 'Data Fine ' + '</td>' + '<td>' + response["end_time"] + '</td>' + '</tr>' +
+        '<tr>' + '<td>' + 'Descrizione ' + '</td>' + '<td>' + response["description"] + '</td>' + '</tr>' +
+        '<tr>' + '<td>' + 'Tipo ' + '</td>' + '<td>' + response["type"] + '</td>' + '</tr>' +
+        '<tr>' + '<td>' + 'Email ' + '</td>' + '<td>' + response["email"] + '</td>' + '</tr>' +
+        '<tr>' + '<td>' + 'Id Corso  ' + '</td>' + '<td>' + response["course_id"] + '</td>' + '</tr>' +
+        '<tr>' + '<td>' + 'Id Classe  ' + '</td>' + '<td style="white-space:pre-wrap; word-wrap:break-word">' + response["classroom_id"] + '</td>' + '</tr>';
+$('#e-table-body-info').append(view);
+
+},
+error: function () {
+console.log('Errore durante il recupero dei dati dal database');
+}
+});
+}
